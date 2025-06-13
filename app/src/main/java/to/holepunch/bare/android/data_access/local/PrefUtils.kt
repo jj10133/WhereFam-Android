b576fd4189b07612795e2c9675ebd5e87494ac79
JJ <>
2025-06-13 13:36:35 -0500

    Fix - Changed the file name and updated to save images to internal storage of the app

---
+++ app/src/main/java/to/holepunch/bare/android/data_access/local/PrefUtils.kt
@@ -0,0 +1,51 @@
+package to.holepunch.bare.android.data_access.local
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
+class PrefUtils(private val context: Context) {
+
+    val Context.userPreferencesDataStore: DataStore<Preferences> by preferencesDataStore(name = "user_info")
+
+    companion object {
+        private val USER_NAME = stringPreferencesKey("user_name")
+        private const val USER_IMAGE_FILE_NAME = "user_profile_image.png"
+    }
+
+    suspend fun saveUserImage(bitmap: Bitmap?) {
+        withContext(Dispatchers.IO) {
+            try {
+                val file = File(context.filesDir, USER_IMAGE_FILE_NAME)
+
+                if (bitmap != null) {
+                    FileOutputStream(file).use { out ->
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+                    }
+                    Log.d("PrefUtils", "Image saved to internal storage: ${file.absolutePath}")
+                }
+            } catch (e: Exception) {
+                Log.e("PrefUtils", "Error saving image to internal storage: ${e.message}", e)
+            }
+        }
+    }
+
+    suspend fun saveUserName(value: String) {
+        context.userPreferencesDataStore.edit { it[USER_NAME] = value }
+    }
+
+    suspend fun getUserName(): String? {
+        return context.userPreferencesDataStore.data.map { it[USER_NAME] }.first()
+    }
+}
