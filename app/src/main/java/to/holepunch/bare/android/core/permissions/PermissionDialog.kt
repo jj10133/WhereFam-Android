d8f7296c5c1feddbe0a6ff5567fe25a1fd5eaa5d
JJ <>
2025-06-12 20:40:25 -0500

    Fix - Reusing location permission logic in the onboarding flow

---
+++ app/src/main/java/to/holepunch/bare/android/core/permissions/PermissionDialog.kt
@@ -0,0 +1,32 @@
+package to.holepunch.bare.android.core.permissions
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PermissionDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(),
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text(text = "Ok")
+            }
+        },
+        title = {
+            Text(
+                text = "Location permissions are needed"
+            )
+        },
+        text = {
+            Text(text = "Please allow us to access your location to find your loved ones")
+        }
+    )
+}
