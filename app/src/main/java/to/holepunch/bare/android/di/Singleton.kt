aa8ded45713f08101b03f08b7da4a6d56eb12cfd
JJ <>
2025-06-13 13:37:10 -0500

    Fix - Using koin for injection

--- app/src/main/java/to/holepunch/bare/android/di/Singleton.kt
+++ app/src/main/java/to/holepunch/bare/android/di/Singleton.kt
@@ -3,22 +3,21 @@ package to.holepunch.bare.android.di
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import to.holepunch.bare.android.core.home.HomeViewModel
+import to.holepunch.bare.android.core.onboarding.ThirdPageViewModel
 import to.holepunch.bare.android.data_access.ipc.IPCProvider
+import to.holepunch.bare.android.data_access.local.PrefUtils
 import to.holepunch.bare.android.manager.LocationManager
 
-
 val appModule = module {
-    single {
-        LocationManager(get())
-    }
+    single { LocationManager(get()) }
 
-    single {
-        IPCProvider.ipc
-    }
+    single { IPCProvider.ipc }
+
+    single { PrefUtils(get()) }
 }
 
 val viewModel = module {
-    viewModel {
-        HomeViewModel(get(), get())
-    }
+    viewModel { HomeViewModel(get(), get()) }
+
+    viewModel { ThirdPageViewModel(get(), get()) }
 }
