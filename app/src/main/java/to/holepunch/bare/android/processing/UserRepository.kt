a5021c1d6ba48954f052acd40bd2469ba06dcd48
JJ <>
2025-06-25 16:19:52 -0500

    Added UserRepository.kt

---
+++ app/src/main/java/to/holepunch/bare/android/processing/UserRepository.kt
@@ -0,0 +1,15 @@
+package to.holepunch.bare.android.processing
+
+import kotlinx.coroutines.flow.StateFlow
+
+
+data class User(val id: String, val name: String? = null, val publicKey: String? = null)
+data class LocationData(val id: String, val name: String, val latitude: Double, val longitude: Double)
+
+interface UserRepository {
+    val currentPublicKey: StateFlow<String>
+
+    suspend fun requestPublicKey()
+
+    fun updatePublicKey(key: String)
+}
