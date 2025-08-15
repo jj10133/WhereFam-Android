ae9817e659c35857da8355cddb3f78e71d71ece9
JJ <warmachinesocial@skiff.com>
2025-08-15 14:13:16 -0500

    Added Peer.kt

---
+++ app/src/main/java/to/holepunch/bare/android/data/local/Peer.kt
@@ -0,0 +1,13 @@
+package to.holepunch.bare.android.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Peer(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double
+)
