2d65e2a8850ba6dc22cf817f106a49f47d0a1a7b
JJ <warmachinesocial@skiff.com>
2025-08-16 18:39:38 -0500

    Fix - Removed the usages of maintaining temp list to show updates on UI

--- app/src/main/java/to/holepunch/bare/android/data/UserRepository.kt
+++ app/src/main/java/to/holepunch/bare/android/data/UserRepository.kt
@@ -3,16 +3,11 @@ package to.holepunch.bare.android.data
 import kotlinx.coroutines.flow.StateFlow
 
 
-data class LocationData(val id: String, val name: String, val latitude: Double, val longitude: Double)
-
 interface UserRepository {
     val currentPublicKey: StateFlow<String>
-    val locationUpdates: StateFlow<List<LocationData>>
 
     suspend fun requestPublicKey()
     suspend fun joinPeer(key: String)
 
     fun updatePublicKey(key: String)
-
-    fun addLocationUpdate(location: LocationData)
 }
--- app/src/main/java/to/holepunch/bare/android/data/ipc/UserRepositoryImpl.kt
+++ app/src/main/java/to/holepunch/bare/android/data/ipc/UserRepositoryImpl.kt
@@ -1,18 +1,11 @@
 package to.holepunch.bare.android.data.ipc
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
-import to.holepunch.bare.android.data.LocationData
 import to.holepunch.bare.android.data.UserRepository
 import to.holepunch.bare.android.data.ipc.IPCUtils.writeAsync
 import to.holepunch.bare.android.data.local.GenericAction
@@ -24,15 +17,6 @@ class UserRepositoryImpl(private val ipc: IPC) : UserRepository {
 
     private val _currentPublicKey = MutableStateFlow("")
     override val currentPublicKey: StateFlow<String> = _currentPublicKey.asStateFlow()
-
-    private val _locationUpdatesMap = MutableStateFlow<Map<String, LocationData>>(emptyMap())
-    override val locationUpdates: StateFlow<List<LocationData>> = _locationUpdatesMap
-        .map { it.values.toList() }
-        .stateIn(
-            scope = CoroutineScope(Dispatchers.Default),
-            started = SharingStarted.Lazily,
-            initialValue = emptyList()
-        )
 
     override suspend fun requestPublicKey() {
         val dynamicData = buildJsonObject {}
@@ -55,11 +39,5 @@ class UserRepositoryImpl(private val ipc: IPC) : UserRepository {
 
     override fun updatePublicKey(key: String) {
         _currentPublicKey.value = key
-    }
-
-    override fun addLocationUpdate(location: LocationData) {
-       _locationUpdatesMap.update { currentMap ->
-           currentMap + (location.id to location)
-       }
     }
 }
