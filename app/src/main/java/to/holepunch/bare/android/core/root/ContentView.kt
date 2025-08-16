5bac6954b582f049a6773f907464b3ac692da56c
JJ <warmachinesocial@skiff.com>
2025-08-16 18:48:04 -0500

    Fix - Added paywall ui

--- app/src/main/java/to/holepunch/bare/android/core/root/ContentView.kt
+++ app/src/main/java/to/holepunch/bare/android/core/root/ContentView.kt
@@ -5,6 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.models.StoreTransaction
+import com.revenuecat.purchases.ui.revenuecatui.PaywallDialog
+import com.revenuecat.purchases.ui.revenuecatui.PaywallDialogOptions
+import com.revenuecat.purchases.ui.revenuecatui.PaywallListener
 import to.holepunch.bare.android.core.home.HomeView
 import to.holepunch.bare.android.core.onboarding.*
 
@@ -27,6 +32,30 @@ fun ContentView(navController: NavHostController, startDestination: String) {
 
         composable("Home") {
             HomeView()
+        }
+
+        composable("Paywall") {
+            PaywallDialog(
+                paywallDialogOptions = PaywallDialogOptions.Builder()
+                    .setDismissRequest {
+                        navController.popBackStack()
+                    }
+                    .setListener(
+                        object : PaywallListener {
+                            override fun onPurchaseCompleted(
+                                customerInfo: CustomerInfo,
+                                storeTransaction: StoreTransaction
+                            ) {
+                                super.onPurchaseCompleted(customerInfo, storeTransaction)
+                            }
+
+                            override fun onRestoreCompleted(customerInfo: CustomerInfo) {
+                                super.onRestoreCompleted(customerInfo)
+                            }
+                        }
+                    )
+                    .build()
+            )
         }
     }
 }
